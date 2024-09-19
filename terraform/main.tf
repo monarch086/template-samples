08f1b47b9417c2ce0feaea56600a3dafd4734fb9
Oleksandr Barsuk <oleksandr.barsuk@acropolium.com>
2024-09-19 14:00:46 +0300

    terraform changes

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+}
