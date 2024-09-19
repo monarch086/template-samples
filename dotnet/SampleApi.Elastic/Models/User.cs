6698b0fa0de5e95e427fb6a7864ae6560313cc99
Oleksandr Barsuk <oleksandr.barsuk@acropolium.com>
2024-09-19 18:08:29 +0300

    [SampleApi.Elastic] Updates

--- dotnet/SampleApi.Elastic/Models/User.cs
+++ dotnet/SampleApi.Elastic/Models/User.cs
@@ -8,7 +8,7 @@ namespace SampleApi.Elastic.Models
     public class User
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
---
+++ dotnet/SampleApi.Elastic/README.md
@@ -0,0 +1,14 @@
+# SampleApi.Elastic
+
+Source Youtube video: <https://youtu.be/FTqN7pB5DXM?si=E5UZw19M1cKfoaKx>
+
+## Run
+
+```powershell
+docker compose up -d
+dotnet run
+```
+
+## Kibana Management Page
+
+<http://localhost:5601>
