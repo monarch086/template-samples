a18c6c3c7e57ee2e4018ec0313a34998e6bc3b3c
Oleksandr Barsuk <oleksandr.barsuk@acropolium.com>
2025-02-09 00:51:03 +0200

    Add @ng-select/ng-select

--- ng-select/select-example-19-st/src/app/app.component.ts
+++ ng-select/select-example-19-st/src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgSelectComponent } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NgSelectComponent,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'select-example-19-st';
+
+  public selectedCar: number | null = null;
+
+  public cars = [
+      { id: 1, name: 'Volvo' },
+      { id: 2, name: 'Saab' },
+      { id: 3, name: 'Opel' },
+      { id: 4, name: 'Audi' },
+  ];
 }
